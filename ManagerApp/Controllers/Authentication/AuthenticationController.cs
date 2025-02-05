5e753d9675bb43a943b285638a4b6c37a50a3f5a
DmitriiBartash <dmitrii.bartas1@isa.utm.md>
2025-02-05 12:16:36 +0200

    chore: создана ветка develop, настроен .gitignore, начало настройки ManagerApp

---
+++ ManagerApp/Controllers/Authentication/AuthenticationController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ManagerApp.Controllers.Authentication
+{
+    public class AuthenticationController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
