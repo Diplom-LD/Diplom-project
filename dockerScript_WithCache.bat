88999d6418298f7c85c5f50f567d115fe694f6c2
Leomir <leomir2002@gmail.com>
2025-04-01 01:33:20 +0300

    autommated migrations

--- dockerScript_WithCache.bat
+++ dockerScript_WithCache.bat
@@ -2,3 +2,20 @@
 docker-compose down
 docker-compose build
 docker-compose up -d
+
+echo MIGRATIONS BLEATI!
+
+
+cd AuthService
+set "folder=Migrations"
+if exist "%folder%\" (
+rmdir /s /q "%folder%"
+)
+dotnet ef migrations add InitialCreateAuthService
+cd ..
+cd OrderService
+set "folder=Migrations"
+if exists %folder\% (
+rmdif /s /q "%folder%"
+)
+dotnet ef migrations add InitialCreateOrderService --context OrderDbContext
